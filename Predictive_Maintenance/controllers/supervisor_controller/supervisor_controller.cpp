f5dc9ba8da722f23251e0f213d681cc8f056c7c9
diana-catana <dia.catana@gmail.com>
2024-09-20 12:47:22 +0200

    Added supervisor, reading accelerometer data from file

--- Predictive_Maintenance/controllers/supervisor_controller/supervisor_controller.cpp
+++ Predictive_Maintenance/controllers/supervisor_controller/supervisor_controller.cpp
@@ -1,52 +1,101 @@
 // File:          supervisor_controller.cpp
 // Date:
-// Description:
+// Description:   Supervisor script to read and save accelerometer data
 // Author:
 // Modifications:
 
-// You may need to add webots include files such as
-// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
-// and/or to add some other includes
-#include <webots/Robot.hpp>
+#include <webots/Supervisor.hpp>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
+using namespace std;
+
+// Function to read accelerometer data from a file
+vector<vector<double>> readAccelerometerData(const string &filename) {
+  vector<vector<double>> data;
+  ifstream file(filename);
+  string line;
+
+  // Check if the file opened successfully
+  if (!file.is_open()) {
+    cerr << "Error: Could not open the file " << filename << endl;
+    return data;
+  }
+
+  // Read the file line by line
+  while (getline(file, line)) {
+    stringstream ss(line);
+    string value;
+    vector<double> row;
+
+    // Split the line by tabs and convert to doubles
+    while (getline(ss, value, '\t')) {
+      row.push_back(stod(value));
+    }
+
+    // Add the row to the data vector
+    data.push_back(row);
+  }
+
+  file.close();
+  return data;
+}
 
 // This is the main program of your controller.
-// It creates an instance of your Robot instance, launches its
-// function(s) and destroys it at the end of the execution.
-// Note that only one instance of Robot should be created in
-// a controller program.
-// The arguments of the main function can be specified by the
-// "controllerArgs" field of the Robot node
 int main(int argc, char **argv) {
-  // create the Robot instance.
-  Robot *robot = new Robot();
+  // Create the Supervisor instance.
+  Supervisor *supervisor = new Supervisor();
 
-  // get the time step of the current world.
-  int timeStep = (int)robot->getBasicTimeStep();
+  // Get the time step of the current world.
+  int timeStep = (int)supervisor->getBasicTimeStep();
 
-  // You should insert a getDevice-like function in order to get the
-  // instance of a device of the robot. Something like:
-  //  Motor *motor = robot->getMotor("motorname");
-  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
-  //  ds->enable(timeStep);
+  // File path to the accelerometer data
+  string filePath = "data/capture1_60hz_30vol.txt";  // Adjust path as necessary
+
+  // Read and save the accelerometer data
+  vector<vector<double>> accelerometerData = readAccelerometerData(filePath);
+
+  // Check if data was successfully read
+  if (!accelerometerData.empty()) {
+    cout << "Successfully read accelerometer data with " << accelerometerData.size() << " entries." << endl;
+
+    // Print the data to verify it was read correctly
+    for (size_t i = 0; i < accelerometerData.size(); ++i) {
+      cout << "Entry " << i + 1 << ": ";
+      for (size_t j = 0; j < accelerometerData[i].size(); ++j) {
+        cout << accelerometerData[i][j] << " ";
+      }
+      cout << endl;
+    }
+  } else {
+    cout << "No data read from the file." << endl;
+  }
+
 
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
-  while (robot->step(timeStep) != -1) {
-    // Read the sensors:
-    // Enter here functions to read sensor data, like:
-    //  double val = ds->getValue();
+  int i = 0;
+  while (supervisor->step(timeStep) != -1) {
+    // Simulation step logic can be added here.
+    if(i < int(accelerometerData.size())){
+      cout << "Entry " << i + 1 << ": ";
+      for (size_t j = 0; j < accelerometerData[i].size(); ++j) {
+        cout << accelerometerData[i][j] << " ";
+      }
+      cout << endl;
+    } else {
+      cout << "Out of data";
+    }
+    i++;
 
-    // Process sensor data here.
-
-    // Enter here functions to send actuator commands, like:
-    //  motor->setPosition(10.0);
   };
 
-  // Enter here exit cleanup code.
-
-  delete robot;
+  // Clean up
+  delete supervisor;
   return 0;
 }
